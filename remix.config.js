8adb82477b757f033184f23093beb39fcbc1a8cf
luchanso <luchansodev@gmail.com>
2023-02-05 18:09:04 +0500

    fix: clean project

--- remix.config.js
+++ remix.config.js
@@ -1,24 +1,8 @@
-const { mountRoutes } = require("remix-mount-routes");
-
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
-  assetsBuildDirectory: "public/training/build",
+  assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
-  publicPath: "/training/build/",
-  routes: (defineRoutes) => {
-    // /myapp => app/routes/index.tsx
-    const baseRoutes = mountRoutes("/training", "routes");
-    // /test => app/addins/test/index.tsx
-    // const testRoutes = mountRoutes('/test', 'addins/test')
-
-    // use standard Remix defineRoutes API
-    // /some/path/* => app/addins/catchall.tsx
-
-    const routes = {
-      ...baseRoutes,
-    };
-    return routes;
-  },
+  publicPath: "/build/",
 };
