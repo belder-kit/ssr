1e42c1d6e1fcd00a28e02ee1f737dcc99d976de1
luchanso <luchansodev@gmail.com>
2023-02-22 01:16:34 +0500

    feat: remove form and add navbar

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,38 +1,13 @@
-import { Box, Button, Heading, Input } from "@chakra-ui/react";
+import { Box, Button, Container, Heading, Input } from "@chakra-ui/react";
 import type { ActionArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import { Navbar } from "~/components/ladning/navbar/Navbar";
 import { db } from "~/utils/db.server";
 
 export const loader = async () => {
-  console.log("loader");
   return json({
     users: await db.user.findMany(),
-  });
-};
-
-export const action = async ({ request }: ActionArgs) => {
-  const form = await request.formData();
-  const userName = form.get("name");
-  const email = form.get("email");
-
-  if (
-    !userName ||
-    !email ||
-    typeof userName !== "string" ||
-    typeof email !== "string"
-  ) {
-    throw new Error(`Form not submitted correctly.`);
-  }
-
-  const user = await db.user.create({
-    data: {
-      email,
-      name: userName,
-    },
-  });
-  return json({
-    ok: 200,
   });
 };
 
@@ -40,33 +15,16 @@ export default function Index() {
   const data = useLoaderData<typeof loader>();
 
   return (
-    <Box m="4">
-      <Heading>Users</Heading>
-      <Box width="300px">
-        <form method="post">
-          <div>
-            <label>
-              Name: <Input type="text" name="name" />
-            </label>
-          </div>
-          <div>
-            <label>
-              Email: <Input type="text" name="email" />
-            </label>
-          </div>
-          <div>
-            <Button type="submit" className="button">
-              Add
-            </Button>
-          </div>
-        </form>
+    <Container maxW="container.lg">
+      <Navbar />
+      <Box m="4">
+        <Heading>Users</Heading>
+        {data.users.map((user) => (
+          <Box key={user.id}>
+            {user.name}, {user.email}
+          </Box>
+        ))}
       </Box>
-
-      {data.users.map((user) => (
-        <Box key={user.id}>
-          {user.name}, {user.email}
-        </Box>
-      ))}
-    </Box>
+    </Container>
   );
 }
