3066e4a54f6acc00599b2b275fd8814aeff318d0
luchanso <luchansodev@gmail.com>
2023-02-22 01:40:35 +0500

    fix: dark mode

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,6 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, cookieStorageManagerSSR } from "@chakra-ui/react";
 import { withEmotionCache } from "@emotion/react";
-import type { MetaFunction } from "@remix-run/node";
+import type { LoaderFunction, MetaFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -8,8 +8,9 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useMemo } from "react";
 import { ClientStyleContext, ServerStyleContext } from "./utils/context";
 import { theme } from "./utils/theme";
 
@@ -19,6 +20,13 @@ export const meta: MetaFunction = () => ({
   viewport: "width=device-width,initial-scale=1",
 });
 
+export const loader: LoaderFunction = async ({ request }) => {
+  return request.headers.get("cookie") ?? "";
+};
+
+const DEFAULT_COLOR_MODE: "dark" | "light" | null = "dark";
+const CHAKRA_COOKIE_COLOR_KEY = "chakra-ui-color-mode";
+
 interface DocumentProps {
   children: React.ReactNode;
 }
@@ -27,6 +35,23 @@ const Document = withEmotionCache(
   ({ children }: DocumentProps, emotionCache) => {
     const serverStyleData = useContext(ServerStyleContext);
     const clientStyleData = useContext(ClientStyleContext);
+
+    let cookies = useLoaderData<string>();
+
+    if (typeof document !== "undefined") {
+      cookies = document.cookie;
+    }
+
+    let colorMode = useMemo(() => {
+      let color = getColorMode(cookies);
+
+      if (!color && DEFAULT_COLOR_MODE) {
+        cookies += ` ${CHAKRA_COOKIE_COLOR_KEY}=${DEFAULT_COLOR_MODE}`;
+        color = DEFAULT_COLOR_MODE;
+      }
+
+      return color;
+    }, [cookies]);
 
     // Only executed on client
     useEffect(() => {
@@ -43,7 +68,13 @@ const Document = withEmotionCache(
     }, []);
 
     return (
-      <html lang="en">
+      <html
+        lang="en"
+        {...(colorMode && {
+          "data-theme": colorMode,
+          style: { colorScheme: colorMode },
+        })}
+      >
         <head>
           <Meta />
           <Links />
@@ -55,8 +86,17 @@ const Document = withEmotionCache(
             />
           ))}
         </head>
-        <body>
-          {children}
+        <body
+          {...(colorMode && {
+            className: `chakra-ui-${colorMode}`,
+          })}
+        >
+          <ChakraProvider
+            theme={theme}
+            colorModeManager={cookieStorageManagerSSR(cookies)}
+          >
+            {children}
+          </ChakraProvider>
           <ScrollRestoration />
           <Scripts />
           <LiveReload />
@@ -65,6 +105,13 @@ const Document = withEmotionCache(
     );
   }
 );
+
+function getColorMode(cookies: string) {
+  const match = cookies.match(
+    new RegExp(`(^| )${CHAKRA_COOKIE_COLOR_KEY}=([^;]+)`)
+  );
+  return match == null ? void 0 : match[2];
+}
 
 export default function App() {
   return (
