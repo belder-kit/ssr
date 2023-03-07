a8e311c703615b53927a0a74a71aaa032e1525fa
luchanso <luchansodev@gmail.com>
2023-03-08 04:15:42 +0500

    feat: update image and navbar

--- app/components/landing/navbar/DesktopLayout.tsx
+++ app/components/landing/navbar/DesktopLayout.tsx
@@ -1,5 +1,13 @@
 import React from "react";
-import { Box, Button, Container, Flex, Heading, Link } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  Heading,
+  Link,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import { Link as RouterLink } from "@remix-run/react";
 
 type Props = {
@@ -12,15 +20,17 @@ type Props = {
 };
 
 export const DesktopLayout = ({ links, onClickAction, projectName }: Props) => {
+  const bgColor = useColorModeValue("#fffffff9", "#1A202Cf9");
+
   return (
-    <Container maxW="container.lg">
-      <Flex
-        justifyContent="space-between"
-        my="4"
-        alignItems="center"
-        display={["none", "none", "flex"]}
-      >
-        <Flex>
+    <Box position="fixed" top="0" left="0" width="full" bg={bgColor}>
+      <Container maxW="container.lg">
+        <Flex
+          justifyContent="space-between"
+          my="4"
+          alignItems="center"
+          display={["none", "none", "flex"]}
+        >
           <Box mx="3">
             <Heading as="h2" size="md">
               <Link
@@ -34,28 +44,30 @@ export const DesktopLayout = ({ links, onClickAction, projectName }: Props) => {
               </Link>
             </Heading>
           </Box>
-          {links.map(({ title, to }) => (
-            <Box mx="3" key={title}>
-              <Link
-                _hover={{
-                  textDecor: "none",
-                }}
-                fontWeight="semibold"
-                fontSize="md"
-                as={RouterLink}
-                to={to}
-              >
-                {title}
-              </Link>
-            </Box>
-          ))}
+          <Flex>
+            {links.map(({ title, to }) => (
+              <Box mx="3" key={title}>
+                <Link
+                  _hover={{
+                    textDecor: "none",
+                  }}
+                  fontWeight="semibold"
+                  fontSize="md"
+                  as={RouterLink}
+                  to={to}
+                >
+                  {title}
+                </Link>
+              </Box>
+            ))}
+          </Flex>
+          <Box>
+            <Button colorScheme="primary" onClick={onClickAction}>
+              Call to Action
+            </Button>
+          </Box>
         </Flex>
-        <Box>
-          <Button colorScheme="primary" onClick={onClickAction}>
-            Call to Action
-          </Button>
-        </Box>
-      </Flex>
-    </Container>
+      </Container>
+    </Box>
   );
 };
